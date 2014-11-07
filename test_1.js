eec4e0a3cac7ec4ae74c0b77a89d0f5a3690a73a
Yegor Silyutin <silyutiny@gmail.com>
2014-11-07 15:01:54 -0800

    Update test_1.js

--- test_1.js
+++ test_1.js
@@ -5,22 +5,36 @@
 #import "test.js"
 #import "image_assertion.js"
 
-    test("Test - Registration", function(target,app) {
+    test("<<< Test - Registration with valid credentials >>>", function(target,app) {
      
      var window = app.mainWindow();
+	 var target = UIATarget.localTarget();
+	 var app = target.frontMostApp();
+	
      app.logElementTree();
      
          target.frontMostApp().mainWindow().scrollViews()[0].textFields()["Server"].tap();
          target.frontMostApp().mainWindow().scrollViews()[0].textFields()["Server"].tap();
          target.frontMostApp().mainWindow().scrollViews()[0].textFields()["Server"].tap();
+		 
+		 UIALogger.logMessage("<<< Input Name of Server >>>");
+		
          target.frontMostApp().keyboard().typeString("lp-web5.moka5qa.com");
-         
          target.frontMostApp().mainWindow().scrollViews()[0].buttons()["Connect"].tap();
+
+		 target.delay(4);
+		
+		 UIALogger.logMessage("<<< Input valid Username and valid Password >>>");
+
          target.frontMostApp().mainWindow().scrollViews()[0].textFields()["Username"].tap();
          target.frontMostApp().keyboard().typeString("iosuser1");
          target.frontMostApp().mainWindow().scrollViews()[0].secureTextFields()["Password"].tap();
          target.frontMostApp().keyboard().typeString("password");
          target.frontMostApp().mainWindow().scrollViews()[0].buttons()["OK"].tap();
-         target.delay(4);
+
+ 		 UIALogger.logMessage("<<< Wait 15 seconds... >>>");
+
+
+         target.delay(15);
      
 });
